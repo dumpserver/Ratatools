229393914a50ca43ee8db716424c31c73482ea26
dumpserver <dumpserver@hotmail.com>
2021-11-24 01:29:01 -0300

    Install React and Redux dependencies.

---
+++ ios/Pods/Target Support Files/Pods-Ratatools-RatatoolsTests/Pods-Ratatools-RatatoolsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Ratatools_RatatoolsTests : NSObject
+@end
+@implementation PodsDummy_Pods_Ratatools_RatatoolsTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-Ratatools/Pods-Ratatools-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Ratatools : NSObject
+@end
+@implementation PodsDummy_Pods_Ratatools
+@end
